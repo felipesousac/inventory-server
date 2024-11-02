68aa36fc3b24c886cfaa7dbd91251b649cb8a98d
felipesousac <claudioflsousa@gmail.com>
2024-11-02 18:37:29 -0300

    Configure prod env

--- src/main/resources/db/migration/V16__create-table-user-permission.sql
+++ src/main/resources/db/migration/V16__create-table-user-permission.sql
@@ -2,7 +2,6 @@ CREATE TABLE `user_permission` (
   `id_user` BIGINT(20) NOT NULL,
   `id_permission` BIGINT(20) NOT NULL,
   PRIMARY KEY (`id_user`,`id_permission`),
-  UNIQUE KEY `fk_user_permission_permission` (`id_permission`),
-  CONSTRAINT `fk_user_permission` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
-  CONSTRAINT `fk_user_permission_permission` FOREIGN KEY (`id_permission`) REFERENCES `permission` (`id`)
+  CONSTRAINT `fk_user_permission` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_user_permission_permission` FOREIGN KEY (`id_permission`) REFERENCES `permission` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
--- src/main/resources/db/migration/V24__add-foreign-key-items-table.sql
+++ src/main/resources/db/migration/V24__add-foreign-key-items-table.sql
@@ -1,3 +1,2 @@
 ALTER TABLE items
-ADD COLUMN user_id BIGINT(20) AFTER categorie_id,
 ADD FOREIGN KEY (user_id) REFERENCES users(id);
